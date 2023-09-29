50ec4e47397764286fa26b3ce8c0e386c0121d18
howsoRes <144272317+howsoRes@users.noreply.github.com>
2023-09-29 11:47:16 -0400

    fix consistent whitespace

--- howso/direct/client.py
+++ howso/direct/client.py
@@ -4842,10 +4842,10 @@ class HowsoDirectClient(AbstractHowsoClient):
 
         self.howso.auto_analyze_params(
             trainee_id=trainee_id,
-			auto_analyze_enabled=auto_analyze_enabled,
+            auto_analyze_enabled=auto_analyze_enabled,
             analyze_threshold=analyze_threshold,
-			auto_analyze_limit_size=auto_analyze_limit_size,
-			analyze_growth_factor=analyze_growth_factor,
+            auto_analyze_limit_size=auto_analyze_limit_size,
+            analyze_growth_factor=analyze_growth_factor,
             **parameters,
             **kwargs
         )
