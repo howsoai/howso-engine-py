ea5b572463aee8488ff16fae7ad0f4abf17676d3
fulpm <8397318+fulpm@users.noreply.github.com>
2024-06-17 16:03:37 +0000

    20537: Refactor client configuration (#207)
    Refactors client configuration class so that options are well defined
    and extensible.

--- howso/client/configuration.py
+++ howso/client/configuration.py
@@ -1,33 +1,86 @@
+from __future__ import annotations
+
+from collections.abc import Iterable, Mapping
+from pathlib import Path
+import typing as t
+
+from requests.structures import CaseInsensitiveDict
+from typing_extensions import TypeVar
 import yaml
 
 from howso.client.exceptions import HowsoConfigurationError
-from howso.client.feature_flags import FeatureFlags
+from .feature_flags import FeatureFlags
+
+CO = TypeVar("CO", bound="ClientOptions", default="ClientOptions")
 
 
-class HowsoConfiguration:
+class BaseOptions:
+    """Base class for Howso configuration options."""
+
+    key: str = ''
+    """Dot separated key path to configuration object in the yaml."""
+
+    required: set = set()
+    """Required sub keys."""
+
+    def __init__(self, config: t.Optional[Mapping]) -> None:
+        if not isinstance(config, Mapping):
+            raise ValueError('Invalid configuration object.')
+        self._config = CaseInsensitiveDict(config)
+        self.post_init()
+
+    def post_init(self) -> None:
+        """Complete any additional setup or validation."""
+        self.check_required(self.required)
+
+    def check_required(self, required: Iterable[str]):
+        """Check for required keys."""
+        for key in required:
+            if key not in self._config:
+                raise HowsoConfigurationError(f'A value for the configuration option "{self.key}.{key}" is required.')
+
+
+class ClientOptions(BaseOptions):
+    """Representation of the Howso client user configuration options."""
+
+    key = "howso"
+
+    @property
+    def client_class(self) -> str | None:
+        """The import path to a client class to use."""
+        return self._config.get('client')
+
+    @property
+    def client_extra_params(self) -> Mapping[str, t.Any]:
+        """Additional client init parameters."""
+        return self._config.get('client_extra_params') or {}
+
+
+class HowsoConfiguration(t.Generic[CO]):
     """
     Howso client configuration.
 
     Parameters
     ----------
-    config_path : str, optional
-        The path to the user's howso.yml
+    config_path : Path or str, optional
+        The path to the user's howso.yml file.
     verbose : bool, default False
         Set verbose output.
     """
 
-    feature_flags_class = FeatureFlags
+    feature_flags_class: type[FeatureFlags] = FeatureFlags
+    client_config_class: type[ClientOptions] = ClientOptions
 
-    def __init__(self, *args, config_path=None, verbose=False, **kwargs):
+    def __init__(self, config_path: t.Optional[Path | str] = None, *, verbose: bool = False):
         """Initialize the configuration object."""
-        super().__init__(*args, **kwargs)
-        self.howso_config_path = config_path
+        self.howso_config_path = None
         self.verbose = verbose
 
         if config_path is not None:
             try:
-                with open(config_path, 'r') as config:
-                    self.user_config = yaml.safe_load(config)
+                self.howso_config_path = Path(config_path).expanduser()
+                with open(self.howso_config_path, 'r') as config:
+                    self._config = CaseInsensitiveDict(yaml.safe_load(config))
             except yaml.YAMLError as yaml_exception:
                 raise HowsoConfigurationError(
                     'Unable to parse the configuration file located at '
@@ -41,35 +94,20 @@ class HowsoConfiguration:
                     'try again.'
                 ) from exception
         else:
-            self.user_config = {}
+            self._config = CaseInsensitiveDict()
 
-        # Initialize feature flags
-        self.feature_flags = self.feature_flags_class(
-            self.user_config.get('feature_flags'))
+        # Initialize configuration classes
+        try:
+            self.setup()
+        except HowsoConfigurationError as ex:
+            if config_path is not None:
+                raise HowsoConfigurationError(
+                    f'See configuration file located at "{config_path}". {ex.message}', code=ex.code) from ex
+            else:
+                raise HowsoConfigurationError(
+                    f'A configuration file is required.". {ex.message}', code=ex.code) from ex
 
-    def get_user_config_option(self, *args, default=None):
-        """
-        Retrieve a configuration option from the user's howso.yml settings.
-
-        Parameters
-        ----------
-        args : str
-            The path to the option in the configuration data.
-        default : Any, default None
-            The value to default to if not found.
-
-        Returns
-        -------
-        Any
-            The value of the option at the given path.
-        """
-        if len(args) == 0:
-            raise AssertionError('At least one configuration option key '
-                                 'is required.')
-        option = self.user_config
-        for arg in args:
-            try:
-                option = option[arg]
-            except (KeyError, TypeError):
-                return default
-        return option
+    def setup(self):
+        """Setup configuration attributes."""
+        self.feature_flags: FeatureFlags = self.feature_flags_class(self._config.get('feature_flags'))
+        self.client: CO = t.cast(CO, self.client_config_class(self._config.get('howso')))
