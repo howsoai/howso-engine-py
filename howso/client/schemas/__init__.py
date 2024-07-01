755197122fcf123f945dde6e7ab3717188583bb9
Matt Fulp <8397318+fulpm@users.noreply.github.com>
2024-07-01 18:49:41 -0400

    fix import

--- howso/client/schemas/__init__.py
+++ howso/client/schemas/__init__.py
@@ -1,4 +1,4 @@
 from .base import BaseSchema
 from .project import Project, ProjectDict
 from .session import Session, SessionDict
-from .trainee import Trainee, TraineeDict
+from .trainee import Trainee, TraineeDict, TraineePersistence
