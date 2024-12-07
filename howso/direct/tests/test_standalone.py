5b9264bdda46faa213e1e4f2a1bca707e999d9f3
David Maze <david.maze@howso.com>
2024-12-06 19:20:18 -0500

    22329: Transactional support in direct client, MINOR
    Adds runtime options to the direct client.  If you create a trainee as
    
        Trainee(persistence='always', runtime={'transactional': True})
    
    then every operation will append a log entry to the .caml file.  This is
    faster per operation, and all of the data continues to be recorded on
    disk, but the resulting files will be larger than always-persist mode
    without the transactional option.

--- howso/direct/tests/test_standalone.py
+++ howso/direct/tests/test_standalone.py
@@ -1,15 +1,18 @@
+from pathlib import Path
+
 import pytest
 
 from amalgam.api import Amalgam
 from howso.direct import HowsoDirectClient
+from howso.direct.schemas.trainee import TraineeDirectRuntimeOptions
 from howso.utilities.testing import get_configurationless_test_client
 
 
 @pytest.fixture
-def client():
+def client(tmp_path: Path):
     """Direct client instance using latest binaries."""
     return get_configurationless_test_client(client_class=HowsoDirectClient,
-                                             verbose=True, trace=True)
+                                             verbose=True, trace=True, default_persist_path=tmp_path)
 
 
 def test_direct_client(client: HowsoDirectClient):
@@ -27,3 +30,41 @@ def test_direct_client(client: HowsoDirectClient):
 def test_check_name_valid_for_save(client, filename, truthiness):
     """Ensure that the internal function `check_name_valid_for_save` works."""
     assert client.check_name_valid_for_save(filename, clobber=True)[0] == truthiness
+
+
+def test_persistence_always(client: HowsoDirectClient, tmp_path: Path):
+    """Test that persist-always mode creates a file on disk."""
+    trainee = client.create_trainee(persistence='always')
+    trainee_path = tmp_path / f"{trainee.id}.caml"
+    client.set_feature_attributes(trainee.id, {"f": {"type": "nominal"}})
+    assert trainee_path.exists()
+
+
+def test_persistence_always_shrinks(client: HowsoDirectClient, tmp_path: Path):
+    """Test that persist-always mode rewrites a file to maybe be smaller."""
+    trainee = client.create_trainee(persistence='always')
+    trainee_path = tmp_path / f"{trainee.id}.caml"
+    client.set_feature_attributes(trainee.id, {"feature_1": {"type": "nominal"},
+                                               "other_unrelated_feature": {"type": "continuous"}})
+    old_size = trainee_path.stat().st_size
+    client.set_feature_attributes(trainee.id, {"feature_1": {"type": "nominal"}})
+    new_size = trainee_path.stat().st_size
+    # We've deleted a feature so the file should be smaller
+    assert new_size < old_size
+
+
+def test_persistence_always_transactional_grows(client: HowsoDirectClient, tmp_path: Path):
+    """Test that transactional mode makes a file larger."""
+    trainee = client.create_trainee(persistence='always', runtime=TraineeDirectRuntimeOptions(transactional=True))
+    trainee_path = tmp_path / f"{trainee.id}.caml"
+    client.set_feature_attributes(trainee.id, {"feature_1": {"type": "nominal"},
+                                               "other_unrelated_feature": {"type": "continuous"}})
+    old_size = trainee_path.stat().st_size
+    client.set_feature_attributes(trainee.id, {"feature_1": {"type": "nominal"}})
+    # Transactional mode always makes the file larger
+    new_size = trainee_path.stat().st_size
+    assert new_size > old_size
+    client.persist_trainee(trainee.id)
+    # But now saving should compact the file
+    new_new_size = trainee_path.stat().st_size
+    assert new_new_size < old_size
