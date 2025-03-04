0e9d682f3fac63e6037fd650eac64fa8390c19ab
Martin Koistinen <615759+mkoistinen@users.noreply.github.com>
2025-03-04 15:34:34 -0500

    23077: Documents `numerical_precision` in `get_params` (#378)

--- howso/client/base.py
+++ howso/client/base.py
@@ -5419,6 +5419,18 @@ class AbstractHowsoClient(ABC):
         weight_feature : str, optional
             If specified, will find and return the best analyzed hyperparameters
             that were analyzed using this weight feature.
+        numerical_precision : str, optional
+            Sets the preference for performance vs. computational accuracy.
+            Valid values are:
+                - "recompute_precise" : default value, will use fast
+                  computation for finding similar cases but recompute their
+                  exact similarities and influences precisely.
+                - "precise" : will always use high precision computation for
+                  finding similar cases and computing similarities
+                  and influences.
+                - "fast" : will always use a fast approach for all computations
+                  which will use faster, but lower precision
+                  numeric operations.
 
         Returns
         -------
