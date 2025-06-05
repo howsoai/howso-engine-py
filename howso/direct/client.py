0b9484f515b8e543253c434ca156d8f784cb7e0d
howsohazard <143410553+howsohazard@users.noreply.github.com>
2025-06-05 15:36:38 -0400

    23822: Updates to take advantage of new Amalgam permissions, removing the possibility of execution of malicious code intentionally embedded in a trainee (#438)
    Co-authored-by: Cade Mack <24661281+cademack@users.noreply.github.com>

--- howso/direct/client.py
+++ howso/direct/client.py
@@ -479,6 +479,9 @@ class HowsoDirectClient(AbstractHowsoClient):
             status_msg = status.message or "An unknown error occurred"
             raise HowsoError(
                 f'Failed to initialize the Trainee "{trainee_id}": {status_msg}')
+        
+        self.amlg.set_entity_permissions(trainee_id, json_permissions='{"load":true,"store":true}')
+
         self.execute(trainee_id, "initialize", {
             "trainee_id": trainee_id,
             "filepath": str(self._howso_dir) + '/',
@@ -1393,6 +1396,8 @@ class HowsoDirectClient(AbstractHowsoClient):
             status_msg = status.message or "An unknown error occurred"
             raise HowsoError(
                 f'Failed to acquire Trainee "{trainee_id}": {status_msg}')
+
+        self.amlg.set_entity_permissions(trainee_id, json_permissions='{"load":true,"store":true}')
 
         # Cache the trainee details
         trainee = self._get_trainee_from_engine(trainee_id)
