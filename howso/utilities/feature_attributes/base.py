eb557d3d0e73b8d56fbd1f3cb982b762414a4a0b
apbassett <43486400+apbassett@users.noreply.github.com>
2025-06-23 14:16:34 -0400

    Fix

--- howso/utilities/feature_attributes/base.py
+++ howso/utilities/feature_attributes/base.py
@@ -1222,8 +1222,7 @@ class InferFeatureAttributesBase(ABC):
             if feature_attributes[feature_name].get('data_type') == 'formatted_time':
                 continue
             # Check original data type for ints, floats, datetimes
-            orig_type = feature_attributes[feature_name]['original_type']['data_type']
-            #orig_type = feature_attributes[feature_name].get('original_type', {}).get('data_type')
+            orig_type = feature_attributes[feature_name].get('original_type', {}).get('data_type')
             if (orig_type in ['integer', 'numeric'] or 'date_time_format' in
                     feature_attributes[feature_name]):
                 # Get feature bounds
