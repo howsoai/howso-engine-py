934ec884121e1a50fa59fd006e464d59161d0f42
howsoRes <144272317+howsoRes@users.noreply.github.com>
2025-07-24 15:59:14 -0400

    fix test

--- howso/client/tests/test_client.py
+++ howso/client/tests/test_client.py
@@ -683,6 +683,7 @@ class TestClient:
             response = self.client.react_aggregate(
                 trainee.id,
                 prediction_stats_action_feature='penguin',
+                feature_influences_action_feature='penguin',
                 num_samples=1,
                 details={old_key: True}
             )
