0cd44bcbdee0ca1b05217d1b33b2114aa5b5c8ea
apbassett <43486400+apbassett@users.noreply.github.com>
2025-09-05 14:35:01 -0400

    Add try/catch

--- howso/utilities/feature_attributes/abstract_data.py
+++ howso/utilities/feature_attributes/abstract_data.py
@@ -345,8 +345,13 @@ class InferFeatureAttributesAbstractData(InferFeatureAttributesBase):
                 if not time_format:
                     raise ValueError(f'Error computing bounds for {feature_name}: '
                                      f'`date_time_format` must be specified in attributes')
-                min_time, max_time = (
-                    self.data.get_min_max_values(feature_name, datetime_format=time_format))
+                try:
+                    min_time, max_time = (
+                        self.data.get_min_max_values(feature_name, datetime_format=time_format))
+                except TypeError:
+                    # howso-engine-connectors < 2.2.0
+                    min_time, max_time = (
+                        self.data.get_min_max_values(feature_name))
                 # Fractional seconds must be normalized to six decimal places for use with Pandas
                 # when the data is represented as a float
                 if '%f' in time_format and original_type.get('data_type') == FeatureType.NUMERIC.value:
@@ -386,8 +391,13 @@ class InferFeatureAttributesAbstractData(InferFeatureAttributesBase):
                 format_dt = feature_attributes[feature_name].get('date_time_format')
 
                 # Trust that the ADC can handle finding min/max datetimes
-                min_date_obj, max_date_obj = (
-                    self.data.get_min_max_values(feature_name, datetime_format=format_dt))
+                try:
+                    min_date_obj, max_date_obj = (
+                        self.data.get_min_max_values(feature_name, datetime_format=format_dt))
+                except TypeError:
+                    # howso-engine-connectors < 2.2.0
+                    min_date_obj, max_date_obj = (
+                        self.data.get_min_max_values(feature_name))
 
                 # Min/max values from ADC are raw; convert to datetime.time
                 if not isinstance(min_date_obj, datetime.datetime):
