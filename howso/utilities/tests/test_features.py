3fe2092de2ce700001d8b50defa30ca5ccbe1d54
apbassett <43486400+apbassett@users.noreply.github.com>
2025-11-07 14:23:33 -0500

    Add comments

--- howso/utilities/tests/test_features.py
+++ howso/utilities/tests/test_features.py
@@ -329,7 +329,7 @@ def test_date_locale_serialization(
 
 
 def test_boolean_features():
-    """Test that binary nominal and boolean features result in the same output from HSE."""
+    """Test that binary nominal and boolean features result in a nearly equivalent output from HSE."""
     # Columns of all 0s and 1s will be nominal ints by default
     nominal_df = pd.read_csv(monk_path)
     boolean_df = nominal_df.copy()
@@ -337,20 +337,25 @@ def test_boolean_features():
     boolean_df["Is smiling"] = boolean_df["Is smiling"].astype("boolean")
     boolean_df["target"] = boolean_df["target"].astype("boolean")
 
+    # Compute feature attributes
     nominal_attributes = infer_feature_attributes(nominal_df)
     boolean_attributes = infer_feature_attributes(boolean_df)
 
+    # Ensure Pandas knows the boolean DataFrame has boolean types
     assert boolean_attributes["target"]["data_type"] == "boolean"
 
+    # Train
     nominal_trainee = Trainee(features=nominal_attributes)
     nominal_trainee.train(nominal_df)
 
     boolean_trainee = Trainee(features=boolean_attributes)
     boolean_trainee.train(boolean_df)
 
+    # Set action/context features
     action_features = ["target"]
     context_features = [col for col in nominal_df.columns if col not in action_features]
 
+    # Set prediction stats/metrics
     prediction_stats = [
         "accuracy",
         "adjusted_smape",
@@ -377,6 +382,7 @@ def test_boolean_features():
         "feature_robust_accuracy_contributions_permutation": True,
     }
 
+    # Do a react for each data type
     nominal_react = nominal_trainee.react_aggregate(action_features=action_features,
                                                     feature_influences_action_feature="target",
                                                     context_features=context_features,
@@ -395,6 +401,6 @@ def test_boolean_features():
                                                         **metrics,
                                                     })
 
-    # assert data_type is boolean for IFA
+    # Compare metrics within a precision of 3 decimals
     for metric in nominal_react:
         pd.testing.assert_frame_equal(nominal_react[metric], boolean_react[metric], check_exact=False, rtol=3)
