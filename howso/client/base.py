1c51bbc5378c0f4759c8703c32dd1a5ef6a48f5b
Cade Mack <24661281+cademack@users.noreply.github.com>
2025-12-04 11:21:52 -0500

    commas in big numbers

--- howso/client/base.py
+++ howso/client/base.py
@@ -3951,8 +3951,8 @@ class AbstractHowsoClient(ABC):
         num_robust_accuracy_contributions_samples : int, optional
             Total sample size of model to use (using sampling with replacement)
             when computing robust accuracy contributions. Defaults to the
-            smaller of 150000 or (6321 * number of context features).
-            Defaults to 100,000 for value_robust_accuracy_contributions.
+            smaller of 150,000 or (6321 * number of context features).
+            Defaults to 100,000 for all value-details.
         num_robust_influence_samples : int, optional
             Total sample size of model to use (using sampling with replacement)
             when computing robust accuracy contributions and robust prediction
