14b4228053a8b2ceb3dc342daf1e249924856bce
Martin Koistinen <615759+mkoistinen@users.noreply.github.com>
2026-07-13 23:06:07 -0400

    Address review comments

--- howso/engine/trainee.py
+++ howso/engine/trainee.py
@@ -1989,7 +1989,7 @@ class Trainee(BaseTrainee):
             weight_feature=weight_feature,
         )
 
-    @auto_progress("React (series)")
+    @auto_progress("React Series")
     def react_series(
         self,
         *,
@@ -2300,7 +2300,7 @@ class Trainee(BaseTrainee):
         else:
             raise ValueError("Trainee ID is needed for react_series.")
 
-    @auto_progress("React (stationary)")
+    @auto_progress("React Series (stationary)")
     def react_series_stationary(
         self,
         action_features: Collection[str],
